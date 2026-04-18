6b81df6f4bc2f47f358582337105d9a57cd957f8
kdu <k4dudev@gmail.com>
2026-04-18 13:25:12 -0300

    ajuste db

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-    Process.env.SUPABASE_URL,
-    Process.env.SUPABASE_KEY
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
 );
 
 export default supabase;
 
-
+/* 
 // const supabase = createClient(
 //     "https://ipuuchygackymxqewwmq.supabase.co",
 //     "sb_publishable_m_aPgRepceu0ZuSB6WWtpg_rNdv_aHY"
 // );
+*/
